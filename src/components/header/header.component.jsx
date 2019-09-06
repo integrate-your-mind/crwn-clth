2f0dd122972b07e248973e3a4dddbd64ba977d81
Roman <rmnlaiacona@gmail.com>
2019-09-06 15:22:11 -0400

    added redux, implemented userReducer and userActions to header, and app components.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,30 +1,40 @@
-import React from 'react'; 
-import {Link} from 'react-router-dom'; 
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import {auth } from '../../firebase/firebase.utils';
+import { auth } from "../../firebase/firebase.utils";
 
-import {ReactComponent as Logo} from '../../assets/crown.svg';
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-import './header.styles.scss'; 
+import "./header.styles.scss";
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo"></Logo>
     </Link>
     <div className="options">
-      <Link className="option" to="/shop">SHOP</Link>
-      <Link className="option" to="/shop">CONTACT</Link>
-      {
-        currentUser ? (
-        <div className='option' onClick={()=>auth.signOut()}>
-        SIGN OUT
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
-):(
-        <Link className='option' to='/signin'>SIGN IN</Link>)
-      }
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
   </div>
 );
 
-export default Header; 
+const mapStateToPropes = state => ({
+  currentUser:state.user.currentUser
+});
+
+export default connect(mapStateToPropes)(Header);
