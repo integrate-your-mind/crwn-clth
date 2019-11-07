53d5a51f8aecd88339e0fb4c6a443656361cb65b
Roman Mondello-Laiacona <rflaiaco@bhcc.edu>
2019-11-07 00:56:02 -0500

    fixed service worker

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,7 @@ import * as serviceWorker from './serviceWorker';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+
 import './index.css';
 import App from './App';
 
